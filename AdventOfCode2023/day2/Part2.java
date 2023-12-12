827ecf83f9e07c493444b2a2567fa53e367ab08d
Whiddon Sibdhannie <whiddon3@gmail.com>
2023-12-11 22:38:30 -0500

    Day 2 Part 1 and 2

---
+++ AdventOfCode2023/day2/Part2.java
@@ -0,0 +1,74 @@
+package AdventOfCode2023.day2;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Part2 {
+
+    public static final String RED = "red";
+    public static final String GREEN = "green";
+    public static final String BLUE = "blue";
+
+    public static Map<String, Integer> parseGame(String[] gameSetStrings) {
+        Map<String, Integer> game = new HashMap<String, Integer>();
+        for (String set : gameSetStrings) {
+            String[] draws = set.split(",");
+            for (String draw : draws) {
+                String[] drawInfo = draw.trim().split(" ");
+                String colorName = drawInfo[1];
+                Integer highestCount = game.get(drawInfo[1]) != null
+                        ? Math.max(game.get(drawInfo[1]), Integer.parseInt(drawInfo[0]))
+                        : Integer.parseInt(drawInfo[0]);
+                game.put(colorName, highestCount);
+            }
+        }
+        return game;
+    }
+
+    public static ArrayList<Map<String, Integer>> parseLines(String filePath) {
+        ArrayList<Map<String, Integer>> games = new ArrayList<Map<String, Integer>>();
+        List<String> lines = Collections.emptyList();
+        try {
+            lines = Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String line : lines) {
+            String[] gameSetStrings = line.split(":")[1].split(";");
+            games.add(parseGame(gameSetStrings));
+        }
+        // System.out.println(String.format("Parsed %d games", games.size()));
+        // System.out.println(games);
+        return games;
+    }
+
+    private static Integer calculatePowerSetSum(ArrayList<Map<String, Integer>> games) {
+        Integer totalSum = 0;
+
+        for (int i = 0; i < games.size(); i++) {
+            Map<String, Integer> game = games.get(i);
+            Integer redCt = game.get(RED) != null ? game.get(RED) : 0;
+            Integer greenCt = game.get(GREEN) != null ? game.get(GREEN) : 0;
+            Integer blueCt = game.get(BLUE) != null ? game.get(BLUE) : 0;
+            System.out.println(String.format("Red %d, green %d, blue %d", redCt, greenCt,
+                    blueCt));
+            totalSum += (redCt * greenCt * blueCt);
+        }
+        return totalSum;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Map<String, Integer>> games = parseLines("AdventOfCode2023\\day2\\input.txt");
+        Integer totalSum = calculatePowerSetSum(games);
+        System.out.println(String.format("%d is the Power Set sum", totalSum));
+    }
+
+}
